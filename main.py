16ca3b99ae044923ab49aa1edaa5b523ac3329a5
Tallyson Ruan Neves <122747621+Tallyson-Ruan-Neves@users.noreply.github.com>
2023-08-06 17:49:59 -0300

    atualização main.py
    implemetação de todo o jogo de maneira simples!

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+try:
+  import random
+  
+  num = random.randint(0,100)
+  print("_"*4+"Jogo de adivinhaçao"+"_"*4)
+  
+  ganhou = False 
+  while not ganhou:
+    num_chute = int(input("Chute um valor entre [0, 100]: "))
+    if num == num_chute:
+      print(f"Voçe ganhou o numero era {num}!")
+      ganhou = True
 
+    elif num > num_chute:
+      print("Errrado, mais!")
+      
+    elif num < num_chute:
+      print("Errrado, menos!")
+
+except:
+  
+  print("Nao foi possivel executar o JOGO!")
